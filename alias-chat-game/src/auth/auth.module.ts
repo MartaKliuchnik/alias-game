fe0af4d89b0471ff9c0e774e8a38e5c009e6c42e
Aliko-XIII <tektov2004@gmail.com>
2024-09-26 10:56:09 +0300

    added mongoose import to modules

--- alias-chat-game/src/auth/auth.module.ts
+++ alias-chat-game/src/auth/auth.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { Auth, AuthSchema } from './entities/auth.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Auth.name, schema: AuthSchema }]),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
