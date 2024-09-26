cef0f03bcbecf2b4408d4be5e785fdd024d6e3e6
Aliko-XIII <tektov2004@gmail.com>
2024-09-26 09:50:31 +0300

    added Schema exports

--- alias-chat-game/src/chats/entities/chat.entity.ts
+++ alias-chat-game/src/chats/entities/chat.entity.ts
@@ -2,3 +2,5 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 @Schema()
 export class Chat extends Document {}
+
+export const ChatSchema = SchemaFactory.createForClass(Chat);
--- alias-chat-game/src/users/entities/user.entity.ts
+++ alias-chat-game/src/users/entities/user.entity.ts
@@ -2,3 +2,5 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 @Schema()
 export class User extends Document{}
+
+export const UserSchema = SchemaFactory.createForClass(User);
