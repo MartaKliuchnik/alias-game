20776ca6f556ea843705bba9bfada712980ee013
Marta <martasagun95@gmail.com>
2024-09-26 20:01:33 +0200

    feat: add User schema, DTOs, and connect MongoDB to project

--- alias-chat-game/src/main.ts
+++ alias-chat-game/src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
   app.setGlobalPrefix('api/v1');
-  await app.listen(8080);
+
+  const port = configService.get<number>('APP_PORT', 3000);
+  await app.listen(port);
 }
 bootstrap();
--- alias-chat-game/src/users/users.controller.ts
+++ alias-chat-game/src/users/users.controller.ts
@@ -6,13 +6,23 @@ import {
   Param,
   Delete,
   ParseIntPipe,
+  UsePipes,
+  ValidationPipe,
+  Post,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async create(@Body() body: CreateUserDto) {
+    const { username, password } = body;
+    return this.usersService.createUser(username, password);
+  }
 
   @Get() // /api/v1/users
   findAll() {
@@ -24,14 +34,6 @@ export class UsersController {
     return this.usersService.findOne(id);
   }
 
-  @Patch(':id') // /api/v1/users/{userId}
-  update(
-    @Param('id', ParseIntPipe) id: number,
-    @Body() updateUserDto: UpdateUserDto,
-  ) {
-    return this.usersService.update(id, updateUserDto);
-  }
-
   @Delete(':id') // /api/v1/users/{userId}
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.remove(id);
@@ -40,5 +42,19 @@ export class UsersController {
   @Get() // /api/v1/users/leaderboard
   findLeaderboard() {
     return this.usersService.findLeaderboard();
+  }
+
+  // @UsePipes(
+  //   new ValidationPipe({
+  //     whitelist: true,
+  //     forbidNonWhitelisted: true,
+  //   }),
+  // )
+  @Patch(':id') // /api/v1/users/{userId}
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return this.usersService.update(id, updateUserDto);
   }
 }
