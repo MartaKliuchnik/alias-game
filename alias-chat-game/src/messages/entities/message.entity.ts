a832dd5c6f9a4753f5387734a9ca1193366f93f6
sxlwrl <vadymkazak2019@gmail.com>
2024-09-27 15:21:37 +0300

    [change] added message entity

--- alias-chat-game/src/messages/entities/message.entity.ts
+++ alias-chat-game/src/messages/entities/message.entity.ts
@@ -1,6 +1,18 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { HydratedDocument, Types } from 'mongoose';
+
+export type MessageDocument = HydratedDocument<Message>;
+
 @Schema()
-export class Message extends Document {}
+export class Message {
+  @Prop({ type: Types.ObjectId, ref: 'users', required: true })
+  userId: Types.ObjectId;
+
+  @Prop({ type: String, required: true })
+  text: string;
+
+  @Prop({ type: Date, default: Date.now })
+  timestamp: Date;
+}
 
 export const MessageSchema = SchemaFactory.createForClass(Message);
