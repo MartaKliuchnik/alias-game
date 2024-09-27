cb55fc8c5983f5ec99999b0cb17d07b6d06013f8
sxlwrl <vadymkazak2019@gmail.com>
2024-09-27 15:21:48 +0300

    [change] added room entity

--- alias-chat-game/src/rooms/entities/room.entity.ts
+++ alias-chat-game/src/rooms/entities/room.entity.ts
@@ -1,6 +1,31 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { HydratedDocument, Types } from 'mongoose';
+
+export type RoomDocument = HydratedDocument<Room>;
+
 @Schema()
-export class Room extends Document{}
+export class Room {
+  @Prop({ type: String, required: true })
+  name: string;
+
+  @Prop({ type: [Types.ObjectId], required: true, default: [] })
+  joinedUsers: Types.ObjectId[];
+
+  @Prop({ type: [Types.ObjectId], required: true })
+  teams: Types.ObjectId[];
+
+  @Prop({ type: Date, default: Date.now })
+  createdAt: Date;
+
+  @Prop({
+    type: Number,
+    required: true,
+    validate: {
+      validator: Number.isInteger,
+      message: 'It is not an integer value',
+    },
+  })
+  turnTime: number;
+}
 
 export const RoomSchema = SchemaFactory.createForClass(Room);
