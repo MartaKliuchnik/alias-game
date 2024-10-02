91526a1d0ecf5d56a42b9843ef22232fff1f22e1
Andrii Bibik <111527201+Andrii3777@users.noreply.github.com>
2024-10-02 13:11:46 +0300

    Added logic to get, check answer and description

--- alias-chat-game/src/words/words.controller.ts
+++ alias-chat-game/src/words/words.controller.ts
@@ -15,7 +15,8 @@ import { Types } from 'mongoose';
 
 @Controller('words')
 export class WordsController {
-  constructor(private readonly wordsService: WordsService) {}
+  // eslint-disable-next-line prettier/prettier
+  constructor(private readonly wordsService: WordsService) { }
 
   @Post()
   create(@Body() createWordDto: CreateWordDto): Promise<Word> {
@@ -43,5 +44,33 @@ export class WordsController {
   @Delete(':id')
   remove(@Param('id') id: Types.ObjectId): Promise<{ message: string }> {
     return this.wordsService.remove(id);
+  }
+
+  @Post('random')
+  getRandomWord(
+    @Body() tryedWords: Types.ObjectId[],
+  ): Promise<{ word: Word; tryedWords: Types.ObjectId[] }> {
+    return this.wordsService.getRandomWord(tryedWords);
+  }
+
+  @Post(':id/check-answer')
+  async checkAnswer(
+    @Param('id') wordId: Types.ObjectId,
+    @Body('answer') answer: string,
+  ): Promise<{ correct: boolean }> {
+    const isCorrect = await this.wordsService.checkAnswer(wordId, answer);
+    return { correct: isCorrect };
+  }
+
+  @Post(':id/check-description')
+  async checkDescription(
+    @Param('id') wordId: Types.ObjectId,
+    @Body('description') description: string,
+  ): Promise<{ correct: boolean }> {
+    const isCorrect = await this.wordsService.checkDescription(
+      wordId,
+      description,
+    );
+    return { correct: isCorrect };
   }
 }
