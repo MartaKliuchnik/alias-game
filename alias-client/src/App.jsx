934c591985f2cc8a9a68a32e005a5e2c1e4a1eed
Marta <martasagun95@gmail.com>
2024-10-04 12:10:04 +0200

    client: set up fetching teams and players from the room (getTeamsFromRoom, getPlayersFromRoom)

--- alias-client/src/App.jsx
+++ alias-client/src/App.jsx
@@ -1,79 +1,81 @@
-import { Route, Routes } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "./components/Navbar/Navbar";
-import HomePage from "./components/HomePage/HomePage";
-import LoginPage from "./pages/LoginPage/LoginPage";
-import LeaderBoardPage from "./pages/LeaderBoardPage/LeaderBoardPage";
-import RegisterPage from "./pages/RegisterPage/RegisterPage";
-import DescriberPage from "./pages/DescriberPage/DescriberPage";
-import LeaderPage from "./pages/LeaderPage/LeaderPage";
-import TeamsResultPage from "./pages/TeamsResultPage/TeamsResultPage";
-import Room from "./components/room/room";
-import Profile from "./components/Profile/Profile";
-import Discussion from "./components/Discussion/Discussion";
-import Wait from "./components/Wait/Wait";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar/Navbar';
+import HomePage from './components/HomePage/HomePage';
+import LoginPage from './pages/LoginPage/LoginPage';
+import LeaderBoardPage from './pages/LeaderBoardPage/LeaderBoardPage';
+import RegisterPage from './pages/RegisterPage/RegisterPage';
+import DescriberPage from './pages/DescriberPage/DescriberPage';
+import LeaderPage from './pages/LeaderPage/LeaderPage';
+import TeamsResultPage from './pages/TeamsResultPage/TeamsResultPage';
+import Room from './components/room/room';
+import Profile from './components/Profile/Profile';
+import Discussion from './components/Discussion/Discussion';
+import Wait from './components/Wait/Wait';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import FinalPage from './pages/FinalPage/FinalPage';
 
 export default function App() {
-  return (
-    <main>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<RegisterPage />} />
-        <Route path="login" element={<LoginPage />} />
-        <Route path="leaderboard" element={<LeaderBoardPage />} />
-        <Route path="describer" element={<DescriberPage />} />
-        <Route path="leader" element={<LeaderPage />} />
-        <Route path="teams-result" element={<TeamsResultPage />} />
-        <Route path="room" element={<Room name={"DEMO"} />} />
-        <Route path="home" element={<HomePage />} />
-        <Route path="profile" element={<Profile />} />
-          
-        <Route
-          path="discussion"
-          element={
-            <Discussion
-              teamName={"TestTeam"}
-              description={"Description for word team has to guess."}
-              users={[
-                { username: "User1" },
-                { username: "User2" },
-                { username: "User3" },
-              ]}
-            />
-          }
-        />
-        <Route
-          path="wait-leader"
-          element={
-            <Wait
-              teamName={"TestTeam"}
-              users={[
-                { username: "User1" },
-                { username: "User2" },
-                { username: "User3" },
-              ]}
-              waitTime={10}
-              role={"leader made decision"}
-            />
-          }
-        />
-        <Route
-          path="wait-describer"
-          element={
-            <Wait
-              teamName={"TestTeam"}
-              users={[
-                { username: "User1" },
-                { username: "User2" },
-                { username: "User3" },
-              ]}
-              waitTime={30}
-              role={"describer write description"}
-            />
-          }
-        />
-      </Routes>
-    </main>
-  );
+	return (
+		<main>
+			<Navbar />
+			<Routes>
+				<Route path='/' element={<RegisterPage />} />
+				<Route path='login' element={<LoginPage />} />
+				<Route path='leaderboard' element={<LeaderBoardPage />} />
+				<Route path='describer' element={<DescriberPage />} />
+				<Route path='leader' element={<LeaderPage />} />
+				<Route path='teams-result' element={<TeamsResultPage />} />
+				<Route path='room' element={<Room name={'DEMO'} />} />
+				<Route path='home' element={<HomePage />} />
+				<Route path='profile' element={<Profile />} />
+				<Route path='final-page' element={<FinalPage />} />
+
+				<Route
+					path='discussion'
+					element={
+						<Discussion
+							teamName={'TestTeam'}
+							description={'Description for word team has to guess.'}
+							users={[
+								{ username: 'User1' },
+								{ username: 'User2' },
+								{ username: 'User3' },
+							]}
+						/>
+					}
+				/>
+				<Route
+					path='wait-leader'
+					element={
+						<Wait
+							teamName={'TestTeam'}
+							users={[
+								{ username: 'User1' },
+								{ username: 'User2' },
+								{ username: 'User3' },
+							]}
+							waitTime={10}
+							role={'leader made decision'}
+						/>
+					}
+				/>
+				<Route
+					path='wait-describer'
+					element={
+						<Wait
+							teamName={'TestTeam'}
+							users={[
+								{ username: 'User1' },
+								{ username: 'User2' },
+								{ username: 'User3' },
+							]}
+							waitTime={30}
+							role={'describer write description'}
+						/>
+					}
+				/>
+			</Routes>
+		</main>
+	);
 }
--- alias-client/src/pages/LeaderPage/LeaderPage.jsx
+++ alias-client/src/pages/LeaderPage/LeaderPage.jsx
@@ -1,102 +1,102 @@
-import { useRef, useState } from "react";
-import { Timer } from "../../components/Timer/Timer";
-import { useNavigate } from "react-router-dom";
-import { checkAnswer } from "../../fetchers/checkAnswer"; // Importing the checkAnswer function
+import { useRef, useState } from 'react';
+import { Timer } from '../../components/Timer/Timer';
+import { useNavigate } from 'react-router-dom';
+import { checkAnswer } from '../../fetchers/checkAnswer'; // Importing the checkAnswer function
 
 export default function LeaderPage() {
-  const [leaderWord, setLeaderWord] = useState("");
-  const [isTimeUp, setIsTimeUp] = useState(false);
-  const wordRef = useRef();
-  const navigate = useNavigate();
-  const wordId = "66fbe2e2c4dcc97328d2ed42"; // test wordId !!!
+	const [leaderWord, setLeaderWord] = useState('');
+	const [isTimeUp, setIsTimeUp] = useState(false);
+	const wordRef = useRef();
+	const navigate = useNavigate();
+	const wordId = '66fbe2e2c4dcc97328d2ed42'; // test wordId !!!
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+	const handleSubmit = async (e) => {
+		e.preventDefault();
 
-    // Time runs out
-    if (isTimeUp) {
-      console.log(null); // Submit null if time has run out
-      return;
-    }
+		// Time runs out
+		if (isTimeUp) {
+			console.log(null); // Submit null if time has run out
+			return;
+		}
 
-    // Validate if the input is empty or whitespace-only
-    if (!leaderWord.trim()) {
-      alert("You must provide a valid decision before submitting.");
-      return;
-    }
+		// Validate if the input is empty or whitespace-only
+		if (!leaderWord.trim()) {
+			alert('You must provide a valid decision before submitting.');
+			return;
+		}
 
-    try {
-      // Check if the answer is correct by making the API call
-      const isCorrect = await checkAnswer(leaderWord, wordId);
+		try {
+			// Check if the answer is correct by making the API call
+			const isCorrect = await checkAnswer(leaderWord, wordId);
 
-      if (!isCorrect) {
-        // Alert if the answer is incorrect
-        alert("Your answer is incorrect. Please try again.");
-        return;
-      }
+			if (!isCorrect) {
+				// Alert if the answer is incorrect
+				alert('Your answer is incorrect. Please try again.');
+				return;
+			}
 
-      // If correct, proceed with the submission
-      alert("Your decision has been successfully submitted and recorded.");
-      setLeaderWord("");
-      navigate("/teams-result"); // Redirect to the results page
-    } catch {
-      // Handle error during submission
-      alert("Failed to submit the answer. Please try again later.");
-    }
-  };
+			// If correct, proceed with the submission
+			alert('Your decision has been successfully submitted and recorded.');
+			setLeaderWord('');
+			navigate('/teams-result'); // Redirect to the results page
+		} catch {
+			// Handle error during submission
+			alert('Failed to submit the answer. Please try again later.');
+		}
+	};
 
-  const handleTimeUp = () => {
-    setIsTimeUp(true);
-    setLeaderWord(""); // Clear the input field when time is up
-    alert(
-      "Time is up! You did not submit a decision within the allowed time. No decision has been recorded."
-    );
-    navigate("/team-result"); // Redirect to the results page
-  };
+	const handleTimeUp = () => {
+		setIsTimeUp(true);
+		setLeaderWord(''); // Clear the input field when time is up
+		alert(
+			'Time is up! You did not submit a decision within the allowed time. No decision has been recorded.'
+		);
+		navigate('/teams-result'); // Redirect to the results page
+	};
 
-  return (
-    <div className="container my-5">
-      <section
-        className="row justify-content-center align-items-center gap-5"
-        style={{ marginTop: "8rem" }}
-      >
-        <div className="col-md-6 text-center">
-          <h2 className="mb-4">
-            {isTimeUp ? "Time's up!" : "Answer Before Time Runs Out!"}
-          </h2>
-          <Timer initialCount={10} onTimeUp={handleTimeUp} />
-        </div>
-        <div className="col-md-6">
-          <form onSubmit={handleSubmit} className="p-4 border rounded shadow">
-            <div className="mb-3">
-              <label htmlFor="leaderWord" className="form-label">
-                Your Final Decision
-              </label>
-              <input
-                type="text"
-                id="leaderWord"
-                className="form-control form-control-lg"
-                ref={wordRef}
-                autoComplete="off"
-                onChange={(e) => setLeaderWord(e.target.value)}
-                value={leaderWord}
-                required
-                placeholder={
-                  isTimeUp
-                    ? "Time's up! Can't submit a decision."
-                    : "Write your final decision"
-                }
-              />
-            </div>
-            <button
-              className="btn btn-lg btn-success w-100"
-              disabled={isTimeUp}
-            >
-              Submit Your Answer
-            </button>
-          </form>
-        </div>
-      </section>
-    </div>
-  );
+	return (
+		<div className='container my-5'>
+			<section
+				className='row justify-content-center align-items-center gap-5'
+				style={{ marginTop: '8rem' }}
+			>
+				<div className='col-md-6 text-center'>
+					<h2 className='mb-4'>
+						{isTimeUp ? "Time's up!" : 'Answer Before Time Runs Out!'}
+					</h2>
+					<Timer initialCount={10} onTimeUp={handleTimeUp} />
+				</div>
+				<div className='col-md-6'>
+					<form onSubmit={handleSubmit} className='p-4 border rounded shadow'>
+						<div className='mb-3'>
+							<label htmlFor='leaderWord' className='form-label'>
+								Your Final Decision
+							</label>
+							<input
+								type='text'
+								id='leaderWord'
+								className='form-control form-control-lg'
+								ref={wordRef}
+								autoComplete='off'
+								onChange={(e) => setLeaderWord(e.target.value)}
+								value={leaderWord}
+								required
+								placeholder={
+									isTimeUp
+										? "Time's up! Can't submit a decision."
+										: 'Write your final decision'
+								}
+							/>
+						</div>
+						<button
+							className='btn btn-lg btn-success w-100'
+							disabled={isTimeUp}
+						>
+							Submit Your Answer
+						</button>
+					</form>
+				</div>
+			</section>
+		</div>
+	);
 }
