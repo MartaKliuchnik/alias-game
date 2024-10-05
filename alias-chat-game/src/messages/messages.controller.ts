2a67f90b435c2d0d9b801a351d351a65057ebc89
sxlwrl <vadymkazak2019@gmail.com>
2024-10-05 11:20:00 +0300

    [change] temporarily commented all controller functions

--- alias-chat-game/src/messages/messages.controller.ts
+++ alias-chat-game/src/messages/messages.controller.ts
@@ -15,28 +15,13 @@ import { UpdateMessageDto } from './dto/update-message.dto';
 export class MessagesController {
   constructor(private readonly messagesService: MessagesService) {}
 
-  @Post()
-  create(@Body() createMessageDto: CreateMessageDto) {
-    return this.messagesService.create(createMessageDto);
-  }
+  // @Post()
+  // sendMessage(@Body() createMessageDto: CreateMessageDto) {
+  //   return this.messagesService.createMessage(createMessageDto);
+  // }
 
-  @Get()
-  findAll() {
-    return this.messagesService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.messagesService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateMessageDto: UpdateMessageDto) {
-    return this.messagesService.update(+id, updateMessageDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.messagesService.remove(+id);
-  }
+  // @Get(':teamId')
+  // async getMessages(@Param('teamId') teamId: string) {
+  //   return this.messagesService.getMessageByTeamId(teamId);
+  // }
 }
