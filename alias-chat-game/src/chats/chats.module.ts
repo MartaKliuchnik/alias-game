169d20ae256011ae190435fefd638dac071b0d9c
sxlwrl <vadymkazak2019@gmail.com>
2024-10-05 16:25:37 +0300

    [change] added messages module to imports

--- alias-chat-game/src/chats/chats.module.ts
+++ alias-chat-game/src/chats/chats.module.ts
@@ -4,10 +4,12 @@ import { ChatsService } from './chats.service';
 import { ChatsController } from './chats.controller';
 import { Chat, ChatSchema } from './schemas/chat.schema';
 import { ChatGateway } from './gateways/chat.gateway';
+import {MessagesModule} from "../messages/messages.module";
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Chat.name, schema: ChatSchema }]),
+    MessagesModule
   ],
   controllers: [ChatsController],
   providers: [ChatsService, ChatGateway],
