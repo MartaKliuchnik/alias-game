674a360a002e60ff99e5ecf1b5b20a62ebaffa3d
sxlwrl <vadymkazak2019@gmail.com>
2024-10-05 11:15:59 +0300

    [change] connected chat gateway

--- alias-chat-game/src/chats/chats.module.ts
+++ alias-chat-game/src/chats/chats.module.ts
@@ -3,12 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ChatsService } from './chats.service';
 import { ChatsController } from './chats.controller';
 import { Chat, ChatSchema } from './schemas/chat.schema';
+import { ChatGateway } from './gateways/chat.gateway';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Chat.name, schema: ChatSchema }]),
   ],
   controllers: [ChatsController],
-  providers: [ChatsService],
+  providers: [ChatsService, ChatGateway],
 })
 export class ChatsModule {}
