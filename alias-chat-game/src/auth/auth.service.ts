79e11aed17855b84812f498bb541a9ce8101af77
Aliko-XIII <tektov2004@gmail.com>
2024-10-07 02:14:41 +0300

    Auth module comments

--- alias-chat-game/src/auth/auth.service.ts
+++ alias-chat-game/src/auth/auth.service.ts
@@ -8,15 +8,36 @@ import { RefreshTokenDto } from './dto/refresh-token.dto';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from '../users/users.service';
 
+/**
+ * Service for user's authorization.
+ */
 @Injectable()
 export class AuthService {
   constructor(
+    /**
+     * Model for user's auth data.
+     */
     @InjectModel(Auth.name) private authModel: Model<Auth>,
+    /**
+     * JWT service for work with tokens.
+     */
     private jwtService: JwtService,
+    /**
+     * Service for work with users.
+     */
     private readonly usersService: UsersService,
   ) {}
 
-  async register(createUserDto: CreateUserDto): Promise<any> {
+  /**
+   * Method for registering a new user.
+   * @param {CreateUserDto} createUserDto Request body with user's data.
+   * @returns {Promise<{
+   * message: string;
+   * data: { userId: string } }>} Message and user's ID.
+   */
+  async register(
+    createUserDto: CreateUserDto,
+  ): Promise<{ message: string; data: { userId: string } }> {
     const { userId } = await this.usersService.createUser(createUserDto);
 
     return {
@@ -27,7 +48,22 @@ export class AuthService {
     };
   }
 
-  async login(loginDto: LoginDto) {
+  /**
+   * Method for login into user's account.
+   * @param {LoginDto} loginDto Request body with user's login data.
+   * @returns {Promise<{
+   * message: string;
+   * data: {
+   * access_token: string;
+   * refresh_token: string }}>} Message and user's access and refresh tokens.
+   */
+  async login(loginDto: LoginDto): Promise<{
+    message: string;
+    data: {
+      access_token: string;
+      refresh_token: string;
+    };
+  }> {
     const { username, password } = loginDto;
     const user = await this.usersService.checkAuth(username, password);
 
@@ -43,12 +79,30 @@ export class AuthService {
     };
   }
 
+  /**
+   * Method for logging out of user's session.
+   * @param {string} userId User's ID for logout.
+   * @returns {Promise<{ message: string }>} Message on logout result.
+   */
   async logout(userId: string): Promise<{ message: string }> {
     await this.authModel.deleteOne({ userId });
     return { message: 'User logged out successfully, refresh token deleted.' };
   }
 
-  async refresh(refreshTokenDto: RefreshTokenDto): Promise<any> {
+  /**
+   * Method for refreshing user's access token.
+   * @param {RefreshTokenDto} refreshTokenDto Request body with user's refresh token.
+   * @returns {Promise<{
+   * message: string;
+   * data: {
+   * access_token: string }}>} Message and user's access token.
+   */
+  async refresh(refreshTokenDto: RefreshTokenDto): Promise<{
+    message: string;
+    data: {
+      access_token: string;
+    };
+  }> {
     const { refreshToken } = refreshTokenDto;
 
     const payload = await this.validateRefreshToken(refreshToken);
@@ -63,6 +117,11 @@ export class AuthService {
     };
   }
 
+  /**
+   * Method for generating a new access token.
+   * @param {string} userId User's ID for generating access token.
+   * @returns {string} Access token string.
+   */
   private generateAccessToken(userId: string): string {
     return this.jwtService.sign(
       { userId },
@@ -70,6 +129,11 @@ export class AuthService {
     );
   }
 
+  /**
+   * Method for generating a new refresh token and saving it to the DB.
+   * @param {string} userId User's ID for refresh token.
+   * @returns {Promise<string>} User's refresh token for getting new access tokens.
+   */
   private async generateAndSaveRefreshToken(userId: string): Promise<string> {
     const refreshToken = this.jwtService.sign(
       { userId },
@@ -85,7 +149,14 @@ export class AuthService {
     return refreshToken;
   }
 
-  private async validateRefreshToken(refreshToken: string) {
+  /**
+   * Method for getting refresh token's payload bject.
+   * @param {string} refreshToken - refresh token's string.
+   * @returns {Promise<{userId:string}>} refresh token's payload decoded.
+   */
+  private async validateRefreshToken(
+    refreshToken: string,
+  ): Promise<{ userId: string }> {
     const decoded = this.jwtService.verify(refreshToken, {
       secret: 'AliasSecret',
     });
