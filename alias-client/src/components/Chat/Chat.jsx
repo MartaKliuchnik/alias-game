e340bce1f9e2a8b183314b6a83eec3b98783d04a
sxlwrl <vadymkazak2019@gmail.com>
2024-10-07 23:13:10 +0300

    [change] added that describer can't write in chat
    removed unused parts
    
    team id, team name and role are now props

--- alias-client/src/components/Chat/Chat.jsx
+++ alias-client/src/components/Chat/Chat.jsx
@@ -1,28 +1,24 @@
 import React, { useEffect, useState } from 'react';
-
 import io from 'socket.io-client';
 
 const socket = io('http://localhost:8080');
 
-export default function Chat({getIdFromToken}) {
+// eslint-disable-next-line react/prop-types
+export default function Chat({getIdFromToken, teamId, name, role}) {
     const [messages, setMessages] = useState([]);
     const [text, setText] = useState('');
-    const [team, setTeam] = useState(null);
     const [roomId] = useState('1');
 
     const userId = getIdFromToken();
 
     useEffect(() => {
-        if (team) {
-            socket.emit('joinTeam', {roomId, teamId: team});
+        if (teamId) {
+            socket.emit('joinTeam', {roomId, teamId});
 
             setMessages([]);
 
-            // TODO FIX A PROBLEM WITH ID (CHECK ID IN CHAT)
-
             const messageHandler = (messageHand) => {
-                console.log(userId);
-                if (messageHand.teamId === team) {
+                if (messageHand.teamId === teamId) {
                     setMessages((prevMessages) => {
                         if (!prevMessages.find(msg => msg.text === messageHand.text && msg.userId === messageHand.userId)) {
                             return [...prevMessages, messageHand];
@@ -38,53 +34,43 @@ export default function Chat({getIdFromToken}) {
                 socket.off('receiveMessage');
             };
         }
-    }, [team, roomId]);
+    }, [teamId, roomId]);
 
     const sendMessage = () => {
         if (!text.trim()) return;
-        const newMessage = {userId, text, teamId: team};
+        const newMessage = {userId, text, teamId};
         setMessages((prevMessages) => [...prevMessages, newMessage]);
-        socket.emit('sendMessage', {roomId, teamId: team, text, userId});
+        socket.emit('sendMessage', {roomId, teamId, text, userId});
         setText('');
     };
 
     return (
         <div className="container">
-            <div className="text-center my-4">
-                <h1>Chat Example Debug</h1>
-                <div className="btn-group mb-3">
-                    <button className="btn btn-outline-primary" onClick={() => setTeam('1')}>Select Team 1</button>
-                    <button className="btn btn-outline-secondary" onClick={() => setTeam('2')}>Select Team 2</button>
-                    <button className="btn btn-outline-success" onClick={() => setTeam('3')}>Select Team 3</button>
-                </div>
-            </div>
             <div className="Chat card">
                 <div className="card-body">
-                    <h2 className="card-title">Chat Room Team Id: {team}</h2>
-                    {team && (
-                        <h2 className="card-subtitle mb-2">Selected team: <strong>{team}</strong></h2>
-                    )}
-
+                    <h2 className="card-title">Chat Room Team: {name}</h2>
                     <div className="chat-window border rounded p-3 mb-3">
                         <h4>Chat:</h4>
                         <div className="messages">
                             {messages.map((msg, index) => (
                                 <div key={index}
-                                     className={msg.team === team ? "alert alert-primary" : "alert alert-secondary"}>
+                                     className={msg.team === teamId ? "alert alert-primary" : "alert alert-secondary"}>
                                     <strong>{msg.userId}:</strong> {msg.text}
                                 </div>
                             ))}
                         </div>
-                        <div className="message-input input-group mt-3">
-                            <input
-                                type="text"
-                                placeholder="Enter the message"
-                                className="form-control"
-                                value={text}
-                                onChange={(e) => setText(e.target.value)}
-                            />
-                            <button className="btn btn-primary" onClick={sendMessage}>Send message</button>
-                        </div>
+                        {(role !== 'describer') &&
+                            <div className="message-input input-group mt-3">
+                                <input
+                                    type="text"
+                                    placeholder="Enter the message"
+                                    className="form-control"
+                                    value={text}
+                                    onChange={(e) => setText(e.target.value)}
+                                />
+                                <button className="btn btn-primary" onClick={sendMessage}>Send message</button>
+                            </div>
+                        }
                     </div>
                 </div>
             </div>
