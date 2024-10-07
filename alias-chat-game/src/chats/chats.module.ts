0752920717134fbc0473b1c84c298c0c0c4f2836
sxlwrl <vadymkazak2019@gmail.com>
2024-10-07 20:18:44 +0300

    [change] removed unused components

--- alias-chat-game/src/chats/chats.module.ts
+++ alias-chat-game/src/chats/chats.module.ts
@@ -1,17 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ChatsService } from './chats.service';
-import { ChatsController } from './chats.controller';
 import { Chat, ChatSchema } from './schemas/chat.schema';
 import { ChatGateway } from './gateways/chat.gateway';
-import {MessagesModule} from "../messages/messages.module";
+import { MessagesModule } from '../messages/messages.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Chat.name, schema: ChatSchema }]),
-    MessagesModule
+    MessagesModule,
   ],
-  controllers: [ChatsController],
-  providers: [ChatsService, ChatGateway],
+  providers: [ChatGateway],
 })
 export class ChatsModule {}
