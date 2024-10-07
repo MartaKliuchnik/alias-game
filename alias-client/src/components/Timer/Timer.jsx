38b5ab4915529665d6e644b29033e03ec6dfea0c
Aliko-XIII <tektov2004@gmail.com>
2024-10-07 20:42:08 +0300

    [fix] timer component does not rely on browser resources

--- alias-client/src/components/Timer/Timer.jsx
+++ alias-client/src/components/Timer/Timer.jsx
@@ -1,34 +1,41 @@
 import React, { useState, useEffect, useRef, useCallback } from 'react';
 
+// eslint-disable-next-line react/prop-types
 const Timer = ({ startTime, onTimeOut, small }) => {
 	const [time, setTime] = useState(startTime);
 	const intervalRef = useRef(null);
+	const lastTimeRef = useRef(Date.now());
 	const timeOutCallback = useCallback(onTimeOut, [onTimeOut]);
 
+	const tick = () => {
+		const now = Date.now();
+		const deltaTime = (now - lastTimeRef.current) / 1000; // difference in seconds
+		lastTimeRef.current = now;
+
+		setTime((prevTime) => {
+			const newTime = Math.max(prevTime - deltaTime, 0);
+			if (newTime <= 0) {
+				clearInterval(intervalRef.current);
+				timeOutCallback();
+			}
+			return newTime;
+		});
+
+		// Continue updating
+		intervalRef.current = requestAnimationFrame(tick);
+	};
+
 	useEffect(() => {
-		if (time === 0) {
-			clearInterval(intervalRef.current);
-			timeOutCallback();
-			return;
-		}
+		lastTimeRef.current = Date.now();
+		intervalRef.current = requestAnimationFrame(tick);
 
-		intervalRef.current = setInterval(() => {
-			setTime((prevTime) => {
-				if (prevTime <= 1) {
-					clearInterval(intervalRef.current);
-					return 0;
-				}
-				return prevTime - 1;
-			});
-		}, 1000);
-
-		return () => clearInterval(intervalRef.current);
-	}, [time]);
+		return () => cancelAnimationFrame(intervalRef.current);
+	}, [timeOutCallback]);
 
 	return small ? (
-		<div className='text-white'>Timer {time} sec</div>
+		<div className='text-white'>Timer {Math.ceil(time)} sec</div>
 	) : (
-		<div className='display-4'>{time}</div>
+		<div className='display-4'>{Math.ceil(time)}</div>
 	);
 };
 
