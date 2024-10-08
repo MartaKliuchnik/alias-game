b5693122e607217b8a79331fc37b4d224ca406ed
sxlwrl <vadymkazak2019@gmail.com>
2024-10-08 18:25:13 +0300

    [change] removed mongoose module

--- alias-chat-game/src/chats/chats.module.ts
+++ alias-chat-game/src/chats/chats.module.ts
@@ -1,14 +1,9 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { Chat, ChatSchema } from './schemas/chat.schema';
 import { ChatGateway } from './gateways/chat.gateway';
 import { MessagesModule } from '../messages/messages.module';
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([{ name: Chat.name, schema: ChatSchema }]),
-    MessagesModule,
-  ],
+  imports: [MessagesModule],
   providers: [ChatGateway],
 })
 export class ChatsModule {}
