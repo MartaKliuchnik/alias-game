6c73840b7758aa48e46f53df02f31663ab5d6661
sxlwrl <vadymkazak2019@gmail.com>
2024-10-08 18:24:30 +0300

    [change] implemented saveMessage method

--- alias-chat-game/src/messages/messages.service.ts
+++ alias-chat-game/src/messages/messages.service.ts
@@ -3,7 +3,6 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Message } from './schemas/message.schema';
 import { CreateMessageDto } from './dto/create-message.dto';
-import { UpdateMessageDto } from './dto/update-message.dto';
 
 @Injectable()
 export class MessagesService {
@@ -11,25 +10,7 @@ export class MessagesService {
     @InjectModel(Message.name) private messageModel: Model<Message>,
   ) {}
 
-  // async createMessage(createMessageDto: CreateMessageDto) {
-  //   const { text } = createMessageDto;
-  //   const message = new this.messageModel({ userId: '', text });
-  //   return await message.save();
-  // }
-
-  async getMessageByTeamId(teamId: string) {
-    return this.messageModel.find({ teamId });
-  }
-
-  // TODO fix with CreateMessage
-
-  async saveMessage(data: {
-    roomId: string;
-    teamId: string;
-    text: string;
-    userId: string;
-  }) {
-    const message = new this.messageModel(data);
-    return await message.save();
+  async saveMessage(data: CreateMessageDto) {
+    return await this.messageModel.create(data);
   }
 }
