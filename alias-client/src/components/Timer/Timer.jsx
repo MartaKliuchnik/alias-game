0648320f8ba0782f024fddd9e9a699d85800a984
Aliko-XIII <tektov2004@gmail.com>
2024-10-08 10:04:21 +0300

    changed timer to use react-timer-hook package

--- alias-client/src/components/Timer/Timer.jsx
+++ alias-client/src/components/Timer/Timer.jsx
@@ -1,43 +1,35 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react';
+import React, { useEffect } from 'react';
+import { useTimer } from 'react-timer-hook';
 
 // eslint-disable-next-line react/prop-types
 const Timer = ({ startTime, onTimeOut, small }) => {
-	const [time, setTime] = useState(startTime);
-	const intervalRef = useRef(null);
-	const lastTimeRef = useRef(Date.now());
-	const timeOutCallback = useCallback(onTimeOut, [onTimeOut]);
+  const expiryTimestamp = new Date();
+  expiryTimestamp.setSeconds(expiryTimestamp.getSeconds() + startTime); // set expiry time
 
-	const tick = () => {
-		const now = Date.now();
-		const deltaTime = (now - lastTimeRef.current) / 1000; // difference in seconds
-		lastTimeRef.current = now;
+  const {
+    seconds,
+    minutes,
+    isRunning,
+    start,
+    restart
+  } = useTimer({
+    expiryTimestamp,
+    onExpire: onTimeOut,
+    autoStart: true,
+  });
 
-		setTime((prevTime) => {
-			const newTime = Math.max(prevTime - deltaTime, 0);
-			if (newTime <= 0) {
-				clearInterval(intervalRef.current);
-				timeOutCallback();
-			}
-			return newTime;
-		});
+  // Restart the timer if `startTime` changes
+  useEffect(() => {
+    const newExpiry = new Date();
+    newExpiry.setSeconds(newExpiry.getSeconds() + startTime);
+    restart(newExpiry);
+  }, [startTime, restart]);
 
-		// Continue updating
-		intervalRef.current = requestAnimationFrame(tick);
-	};
-
-	useEffect(() => {
-		lastTimeRef.current = Date.now();
-		intervalRef.current = requestAnimationFrame(tick);
-
-		return () => cancelAnimationFrame(intervalRef.current);
-	// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [timeOutCallback]);
-
-	return small ? (
-		<div className='text-white'>Timer {Math.ceil(time)} sec</div>
-	) : (
-		<div className='display-4'>{Math.ceil(time)}</div>
-	);
+  return small ? (
+    <div className='text-white'>Timer {seconds} sec</div>
+  ) : (
+    <div className='display-4'>{minutes}:{seconds}</div>
+  );
 };
 
 export default React.memo(Timer);
