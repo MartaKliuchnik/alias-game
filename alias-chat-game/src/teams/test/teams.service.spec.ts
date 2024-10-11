0a049c5326d0507c400cbd466baef2956e40bf98
Aliko-XIII <tektov2004@gmail.com>
2024-10-12 00:15:15 +0300

    sets up teams service unit tests

--- alias-chat-game/src/teams/test/teams.service.spec.ts
+++ alias-chat-game/src/teams/test/teams.service.spec.ts
@@ -1,18 +1,52 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getModelToken } from '@nestjs/mongoose';
 import { TeamsService } from '../teams.service';
+import { Team } from '../schemas/team.schema';
+import { createTeamStub, teamArrStub, teamStub } from './stubs/team.stub';
+import { playersStub, userStub } from './stubs/user.stub';
+import { UsersService } from '../../users/users.service';
+import { RoomsService } from '../../rooms/rooms.service';
+import {
+  BadRequestException,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Model } from 'mongoose';
 
 describe('TeamsService', () => {
-  let service: TeamsService;
+  let teamsService: TeamsService;
+  let teamModel: Model<Team>;
+  let usersService: Partial<UsersService>;
+  let roomsService: Partial<RoomsService>;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [TeamsService],
+    usersService = {};
+    roomsService = {};
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      providers: [
+        TeamsService,
+        {
+          provide: getModelToken(Team.name),
+          useValue: {
+            findOne: jest.fn(),
+            create: jest.fn(),
+            find: jest.fn(),
+            findById: jest.fn(),
+            findByIdAndUpdate: jest.fn(),
+            findByIdAndDelete: jest.fn(),
+          },
+        },
+        { provide: UsersService, useValue: usersService },
+        { provide: RoomsService, useValue: roomsService },
+      ],
     }).compile();
 
-    service = module.get<TeamsService>(TeamsService);
+    teamsService = moduleRef.get<TeamsService>(TeamsService);
+    teamModel = moduleRef.get<Model<Team>>(getModelToken(Team.name));
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  afterEach(() => {
+    jest.clearAllMocks();
   });
+
 });
